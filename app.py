47cfdbf63c313c008a6afd0f5f426132a7ea8e0c
Stark <140642307+darthwook@users.noreply.github.com>
2024-11-25 04:26:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -17,18 +17,18 @@ def index():
 
 @app.route('/run_dead_reckoning', methods=['GET'])
 def run_dead_reckoning():
-    '''
     try:
-        # Your code to run the dead reckoning script
         result = "Dead reckoning process completed."
-        return jsonify({'status': 'success', 'message': result}), 200
+        your_data = {'status': 'success', 'message': result}
+        
+        response = jsonify(your_data)
+        response.headers['Content-Type'] = 'application/json'
+        
+        return response  
+    
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
-        '''
-    your_data = 'API'
-    response = jsonify(your_data)
-    response.headers['Content-Type'] = 'application/json'
-return response
 
 if __name__ == "__main__":
     app.run(debug=True)
+
